e47753eeaac7c61be8cf65a51f8fb2d293b137a0
TheBabu <ariq.haqq@gmail.com>
2024-10-20 09:33:23 -0600

    Run multiple data for spin 1/2

--- simple_grape/multiple_run.py
+++ simple_grape/multiple_run.py
@@ -5,8 +5,13 @@ import argparse
 from pathlib import Path
 import numpy as np
 import pandas as pd
+from multiprocessing.pool import Pool
 
 from simple_grape import SimpleGRAPE
+
+#HACK: To allow different functions in multithreading
+def smap(f, *args):
+    return f(*args)
 
 if __name__ == "__main__":
     #Parse system arguments
@@ -14,10 +19,9 @@ if __name__ == "__main__":
     parser.add_argument("--hilbert_dim", nargs="?", default=2)
     parser.add_argument("--interval_min", nargs="?", default=1)
     parser.add_argument("--interval_max", nargs="?", default=10)
-    parser.add_argument("--interval_num_steps", nargs="?", default=100)
     parser.add_argument("--total_time_min", nargs="?", default=0)
     parser.add_argument("--total_time_max", nargs="?", default=10)
-    parser.add_argument("--total_time_num_steps", nargs="?", default=100)
+    parser.add_argument("--total_time_num_steps", nargs="?", default=10)
     parser.add_argument("--drift_param", nargs="?", default=1)
     parser.add_argument("--taylor_truncate_len", nargs="?", default=10)
     parser.add_argument("--num_init_seeds", nargs="?", default=10)
@@ -41,28 +45,29 @@ if __name__ == "__main__":
     interval_list   = np.array(range(INTERVAL_MIN, INTERVAL_MAX + 1))
     total_time_list = np.linspace(TOTAL_TIME_MIN, TOTAL_TIME_MAX, TOTAL_TIME_NUM_STEPS)
 
+    pool = Pool()
     for num_of_intervals in interval_list:
         for total_time in total_time_list:
             for target_state_seed in range(NUM_TARGET_STATE_SEEDS):
-                target_state  = random_statevector(HILBERT_DIMENSION, seed=target_state_seed) #Set seed for reproducibility
+                target_state = random_statevector(HILBERT_DIMENSION, seed=target_state_seed) #Set seed for reproducibility
 
-                #TODO: Multithread
-                for init_seed in range(NUM_INIT_SEEDS):
-                    #Run Simple GRAPE algorithm
-                    simple_grape = SimpleGRAPE(HILBERT_DIMENSION,
-                                               num_of_intervals,
-                                               total_time,
-                                               DRIFT_PARAMETER,
-                                               TAYLOR_TRUNCATE_LEN,
-                                               init_seed,
-                                               INITIAL_STATE,
-                                               target_state)
+                simple_grape_list = [
+                    SimpleGRAPE(HILBERT_DIMENSION,
+                                num_of_intervals,
+                                total_time,
+                                DRIFT_PARAMETER,
+                                TAYLOR_TRUNCATE_LEN,
+                                init_seed,
+                                INITIAL_STATE,
+                                target_state)
+                    for init_seed in range(NUM_INIT_SEEDS)
+                ]
+                simple_grape_run_list = [simple_grape.run for simple_grape in simple_grape_list]
 
-                    (final_cost, theta_x_waveforms, theta_y_waveforms, unitary_list) = simple_grape.run()
-                    
-                    #DEBUG
-                    print(f"{num_of_intervals=:3}, {total_time=:20.17f}, {target_state_seed=:3}, {init_seed=:3}, {final_cost=:20.17f}") 
-                    
+                #Multithread each initial waveform seed
+                pool_result = pool.map(smap, simple_grape_run_list)
+
+                for init_seed, (final_cost, theta_x_waveforms, theta_y_waveforms, unitary_list) in enumerate(pool_result):
                     #Create data path
                     data_path = Path(__file__).parents[1] / "data" / "grape_data" /\
                         f"{HILBERT_DIMENSION}_dim" / f"N_{num_of_intervals}" / f"T_{total_time}" / f"drift_param_{DRIFT_PARAMETER}" /\
@@ -94,7 +99,13 @@ if __name__ == "__main__":
                         "unitary": unitary_list,
                     })
                     unitary_list_df.to_csv(data_path / "unitary_list.csv")
+
+                    #DEBUG
+                    print(f"{num_of_intervals=:3}, {total_time=:20.17f}, {target_state_seed=:3}, {init_seed=:3}, {final_cost=:20.17f}") 
                 
                 #DEBUG
                 print()
+
+    #Close mulithreading pool
+    pool.close()
 
