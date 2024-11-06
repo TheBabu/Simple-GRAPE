Fix gradient bug

graph_util/waveform.py
@@ -28,8 +28,7 @@ if __name__ == "__main__":
     data_path     = Path(__file__).parents[1] / "data" / "waveform_plots" / Path(*FOLDER_PATH.parts[-num_of_params:])
     data_path.mkdir(parents=True, exist_ok=True)
 
-    theta_x_waveforms = pd.concat([pd.Series([theta_waveforms_df["theta_x"][0]]), theta_waveforms_df["theta_x"]])
-    theta_y_waveforms = pd.concat([pd.Series(theta_waveforms_df["theta_y"][0]), theta_waveforms_df["theta_y"]])
+    theta_waveforms = pd.concat([pd.Series([theta_waveforms_df["theta"][0]]), theta_waveforms_df["theta"]])
 
     time_step         = total_time / num_of_intervals
     time_intervals    = np.linspace(0, total_time, num_of_intervals + 1)
@@ -38,27 +37,15 @@ if __name__ == "__main__":
     plt.rc("font",**{"family":"serif","serif":["Palatino"]})
     plt.rc("text", usetex=True)
 
-    #Graph theta_x_waveforms
-    title_str = f"$\\theta_X$ Waveforms (Final Cost: {final_cost})"
+    #Graph theta_waveforms
+    title_str = f"$\\theta$ Waveforms (Final Cost: {final_cost})"
     plt.figure(figsize=(15, 9), linewidth=2 * 1.15)
-    plt.step(time_intervals, theta_x_waveforms, linewidth=2 * 1.15)
+    plt.step(time_intervals, theta_waveforms, linewidth=2 * 1.15)
     plt.title(title_str, fontsize=20 * 1.15, pad=10)
     plt.xlabel("$\Omega t$", fontsize=20 * 1.15)
-    plt.ylabel("$\\theta_X$", fontsize=20 * 1.15)
+    plt.ylabel("$\\theta$", fontsize=20 * 1.15)
     plt.tick_params(labelsize=15 * 1.15)
     plt.ylim(-y_scale_limit, y_scale_limit)
-    plt.savefig(data_path / "theta_x_waveforms_graph.svg")
-    plt.savefig(data_path / "theta_x_waveforms_graph.png")
-
-    #Graph theta_y_waveforms
-    title_str = f"$\\theta_Y$ Waveforms (Final Cost: {final_cost})"
-    plt.figure(figsize=(15, 9), linewidth=2 * 1.15)
-    plt.step(time_intervals, theta_y_waveforms, linewidth=2 * 1.15)
-    plt.title(title_str, fontsize=20 * 1.15, pad=10)
-    plt.xlabel("$\Omega t$", fontsize=20 * 1.15)
-    plt.ylabel("$\\theta_Y$", fontsize=20 * 1.15)
-    plt.tick_params(labelsize=15 * 1.15)
-    plt.ylim(-y_scale_limit, y_scale_limit)
-    plt.savefig(data_path / "theta_y_waveforms_graph.svg")
-    plt.savefig(data_path / "theta_y_waveforms_graph.png")
+    plt.savefig(data_path / "theta_waveforms_graph.svg")
+    plt.savefig(data_path / "theta_waveforms_graph.png")
 

simple_grape/simple_grape.py
@@ -35,24 +35,24 @@ class SimpleGRAPE:
 
         return total_unitary
 
-    def generate_discrete_hamiltonian(self, theta_x, theta_y):
-        x_spin_hamiltonian_term = np.cos(theta_x) * self.x_spin_operator
-        y_spin_hamiltonian_term = np.sin(theta_y) * self.y_spin_operator
+    def generate_discrete_hamiltonian(self, theta):
+        x_spin_hamiltonian_term = np.cos(theta) * self.x_spin_operator
+        y_spin_hamiltonian_term = np.sin(theta) * self.y_spin_operator
         discrete_hamiltonian    = self.drift_hamiltonian_term + x_spin_hamiltonian_term + y_spin_hamiltonian_term
 
         return discrete_hamiltonian
 
-    def generate_unitary_list(self, theta_x_waveforms, theta_y_waveforms):
+    def generate_unitary_list(self, theta_waveforms):
         unitary_list = [
-            Operator(sp.linalg.expm(-1j * self.time_step * self.generate_discrete_hamiltonian(theta_x_waveforms[i], theta_y_waveforms[i])))
-            for i in range(self.num_of_intervals)
+            Operator(sp.linalg.expm(-1j * self.time_step * self.generate_discrete_hamiltonian(theta)))
+            for theta in theta_waveforms
         ]
 
         return unitary_list
 
-    def calculate_cost_and_gradient(self, theta_x_waveforms, theta_y_waveforms, trunacted_taylor_length):
+    def calculate_cost_and_gradient(self, theta_waveforms, trunacted_taylor_length):
         #Generate list of all unitaries at each time with specific ukj
-        unitary_list = self.generate_unitary_list(theta_x_waveforms, theta_y_waveforms)
+        unitary_list = self.generate_unitary_list(theta_waveforms)
 
         #Create total unitary
         total_unitary = self.compose_unitaries(unitary_list) #U_tot = U_N U_N-1 U_N-2 ... U_j ... U_2 U_1
@@ -66,28 +66,26 @@ class SimpleGRAPE:
         self.cost_list.append(cost)
 
         #Calculate gradient
-        x_spin_gradient = [] #del F / del u1j (l = 1)
-        y_spin_gradient = [] #del F / del u2j (l = 2)
+        cost_gradient = [] #del C / del uj
 
         #Calculate the first part of the gradient
         complex_conjugate_expectation = self.target_state.inner(evolved_state).conj() #<psi_t|U_tot|psi_0>*
 
         #Calculate gradient for x_spin and y_spin term
-        for j, (theta_x, theta_y) in enumerate(zip(theta_x_waveforms, theta_y_waveforms)):
+        for j, theta in enumerate(theta_waveforms):
             #Calculate partial derivatives
-            x_spin_partial_derivative = Operator(np.zeros((self.hilbert_dimension, self.hilbert_dimension))) #del Uj / del u1j (l = 1)
-            y_spin_partial_derivative = Operator(np.zeros((self.hilbert_dimension, self.hilbert_dimension))) #del Uj / del u2j (l = 2)
+            unitary_gradient = Operator(np.zeros((self.hilbert_dimension, self.hilbert_dimension))) #del Uj / del uj
 
             for n in range(trunacted_taylor_length):
-                discrete_hamiltonian = self.generate_discrete_hamiltonian(theta_x, theta_y) #cos(theta_x) Jx + sin(theta_y) Jy + beta * Jz^2 
+                discrete_hamiltonian = self.generate_discrete_hamiltonian(theta) #Hj
 
-                x_spin_partial_derivative +=\
-                    (-1j ** n) / (sp.special.factorial(n)) *\
-                    sum((discrete_hamiltonian ** (i - 1)) @ (-np.sin(theta_x) * self.x_spin_operator) @ (discrete_hamiltonian ** (n - 1)) for i in range(1, n + 1))
-                
-                y_spin_partial_derivative +=\
-                    (-1j ** n) / (sp.special.factorial(n)) *\
-                    sum((discrete_hamiltonian ** (i - 1)) @ (np.cos(theta_y) * self.y_spin_operator) @ (discrete_hamiltonian ** (n - 1)) for i in range(1, n + 1))
+                unitary_gradient +=\
+                    ((-1j * self.time_step) ** n) / (sp.special.factorial(n)) *\
+                    sum(
+                        (discrete_hamiltonian ** (i - 1)) @
+                        ((-np.sin(theta) * self.x_spin_operator) + (np.cos(theta) * self.y_spin_operator)) @
+                        (discrete_hamiltonian ** (n - i))
+                    for i in range(1, n + 1))
 
             #Compose front and back slice unitaries
             front_slice_unitaries = unitary_list[0:j]
@@ -96,40 +94,26 @@ class SimpleGRAPE:
             front_slice_total_unitary = self.compose_unitaries(front_slice_unitaries) #U_j-1 ... U_2 U_1
             back_slice_total_unitary  = self.compose_unitaries(back_slice_unitaries) #U_N U_N-1 ... U_j+1
 
-            #U_x = U_N U_N-1 U_N-2 ... del U_j / del u1j ... U_2 U_1
-            x_spin_composed_unitary = front_slice_total_unitary.compose(x_spin_partial_derivative).compose(back_slice_total_unitary)
-            #U_y = U_N U_N-1 U_N-2 ... del U_j / del u2j ... U_2 U_1
-            y_spin_composed_unitary = front_slice_total_unitary.compose(y_spin_partial_derivative).compose(back_slice_total_unitary)
+            #U = U_N U_N-1 U_N-2 ... del U_j / del u1j ... U_2 U_1
+            composed_unitary = front_slice_total_unitary.compose(unitary_gradient).compose(back_slice_total_unitary)
 
-            x_spin_evolved_state = self.initial_state.evolve(x_spin_composed_unitary) #U_x|psi_0>
-            y_spin_evolved_state = self.initial_state.evolve(y_spin_composed_unitary) #U_y|psi_0>
+            evolved_state = self.initial_state.evolve(composed_unitary) #U|psi_0>
 
-            x_spin_composed_expectation = self.target_state.inner(x_spin_evolved_state) #<psi_t|U_x|psi_0>
-            y_spin_composed_expectation = self.target_state.inner(y_spin_evolved_state) #<psi_t|U_y|psi_0>
+            composed_expectation = self.target_state.inner(evolved_state) #<psi_t|U|psi_0>
 
-            #Update x_spin and y_spin gradient (for specific time step)
-            x_spin_gradient.append(-2 * np.real(complex_conjugate_expectation * x_spin_composed_expectation)) #-2 Re{<psi_t|U_tot|psi_0>* <psi_t|U_x|psi_0>}
-            y_spin_gradient.append(-2 * np.real(complex_conjugate_expectation * y_spin_composed_expectation)) #-2 Re{<psi_t|U_tot|psi_0>* <psi_t|U_y|psi_0>}
+            #Update cost gradient (for specific time step)
+            cost_gradient.append(-2 * np.real(complex_conjugate_expectation * composed_expectation)) #-2 Re{<psi_t|U_tot|psi_0>* <psi_t|U|psi_0>}
 
-        #Compose x_spin and y_spin gradients together
-        # gradient = x_spin_gradient + y_spin_gradient
-
-        #Add x_spin and y_spin gradients together
-        gradient = np.array(x_spin_gradient) + np.array(y_spin_gradient)
-
-        return cost, gradient
+        return cost, cost_gradient
     
     def run(self):
-        #HACK: Input the same waveforms list for both theta_x and theta_y waveforms
         cost_and_gradient_func = lambda waveforms : (
-            self.calculate_cost_and_gradient(waveforms[0:self.num_of_intervals],
-                                             waveforms[0:self.num_of_intervals],
-                                             self.truncated_taylor_len)
+            self.calculate_cost_and_gradient(waveforms[0:self.num_of_intervals], self.truncated_taylor_len)
         )
 
         #Initialize theta waveforms randomly from -pi to pi
         rng                     = np.random.default_rng(self.init_seed) #Set seed for reproducibility
-        initial_theta_waveforms = [rng.uniform(low=-np.pi, high=np.pi) for _ in range(self.num_of_intervals)] #HACK: theta_x and theta_y are the same waveforms
+        initial_theta_waveforms = [rng.uniform(low=-np.pi, high=np.pi) for _ in range(self.num_of_intervals)]
 
         #If check grad, return gradient error compared to finite differences at initial waveforms (Don't perform classical optimization)
         if(self.check_grad):
@@ -142,7 +126,7 @@ class SimpleGRAPE:
             return grad_error
 
         #Perform classical optimization
-        bounds           = [(-np.pi, np.pi)] * (self.num_of_intervals) #Bounds are now for when theta_x and theta_y are the same waveforms
+        bounds           = [(-np.pi, np.pi)] * (self.num_of_intervals)
         optimizer_result = sp.optimize.minimize(cost_and_gradient_func,
                                                 x0=initial_theta_waveforms,
                                                 jac=True,
@@ -150,9 +134,8 @@ class SimpleGRAPE:
                                                 bounds=bounds)
 
         final_cost        = optimizer_result.fun
-        theta_x_waveforms = optimizer_result.x[0:self.num_of_intervals]
-        theta_y_waveforms = optimizer_result.x[0:self.num_of_intervals] #HACK: theta_x and theta_y are the same waveforms
-        unitary_list      = self.generate_unitary_list(theta_x_waveforms, theta_y_waveforms)
+        theta_waveforms = optimizer_result.x[0:self.num_of_intervals]
+        unitary_list      = self.generate_unitary_list(theta_waveforms)
 
-        return (final_cost, theta_x_waveforms, theta_y_waveforms, unitary_list)
+        return (final_cost, theta_waveforms, unitary_list)
 